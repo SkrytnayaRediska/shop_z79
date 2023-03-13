03427046a8d9cd90b23cac6c17391445d20af702
SkrytnayaRediska <olga@arusnavi.ru>
2023-03-13 15:05:02 +0300

    change settings

--- shop/settings.py
+++ shop/settings.py
@@ -119,9 +119,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'shop',
-        'USER': 'olga',
+        'USER': 'postgres',
         'PASSWORD': '',
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
